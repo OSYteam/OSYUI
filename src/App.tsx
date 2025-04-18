42813b8cab8274fd65dd461d64fb5d4bcf7760a8
ramalt <ramazanalltuntepe@gmail.com>
2025-04-19 00:28:56 +0300

    feat : New Layout - Drawer menu added to Root Layout. - Theme setting added to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { Box, Button } from '@mui/material'
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
-    <>
-
-      {/* <Box width="100vw" height="100vh" justifyItems="center" alignContent="center"  >
-
-        <Button variant="contained" color="primary" onClick={() => { alert("world!") }}>
-          Hello
-        </Button>
-
-      </Box> */}
-      {/* <Box  bgcolor="yellowgreen" >
-
-      </Box> */}
-
         <AppRoutes />
-    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: 'light',
     primary: {
-      main: '#ffffff',
+      main: '#d6d5d0',
     },
     secondary: {
       main: '#dc004e',
     },
   },
 });
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-
-  {
-    path: "/test",
-    element: <div>Test</div>,
-  },
-]);
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
